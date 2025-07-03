5c2fea400a3eb7edc29bff7332c34dc179e4bd52
shinkibeom <shinkibeoms@naver.com>
2025-07-03 19:00:58 +0900

    feat: 약 클래스 추가

--- src/domain/Medicine.java
+++ src/domain/Medicine.java
@@ -1,10 +1,17 @@
 package domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Medicine {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	private String name;	
+	private int quantity;
+	private int amount;
+	
 }
