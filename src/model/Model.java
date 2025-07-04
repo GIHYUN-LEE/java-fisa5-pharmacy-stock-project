095bbd33e46eda5f4b362223d3f0c1a616ca3ff9
menzzi <minmin3927@naver.com>
2025-07-04 15:42:22 +0900

    feat: 데이터베이스에 약 존재 여부 확인하는 기능 추가

--- src/model/Model.java
+++ src/model/Model.java
@@ -1,10 +1,24 @@
 package model;
 
+import java.util.HashMap;
+
+import domain.Medicine;
+
 public class Model {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private Database db = new Database();
+	
+	private static Model model = new Model();
+	
+	public HashMap<String, Medicine> getMedicineList() {
+		return db.getMedicine();
 	}
-
+	
+	public boolean isExist(String medicineName) throws Exception {
+		HashMap<String, Medicine> medicineList = model.getMedicineList();
+		if(!medicineList.containsKey(medicineName)) {
+			throw new Exception("존재하지 않는 약입니다.");
+		} 
+		return true;
+	}
+	
 }
