7024ba364e8df633a1dbd8ef8835012a68d9e844
GIHYUN-LEE <gihyun506@naver.com>
2025-07-04 20:51:05 +0900

    Prac..

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,14 +1,25 @@
 package controller;
 
+import java.util.HashMap;
 import java.util.Scanner;
-
+import domain.Medicine;
+import model.Model;
 import view.InputView;
 import view.StartView;
 
 public class Controller {
+	
+	Model model = new Model();
+
+	public HashMap<String, Medicine> getMedicineList() {  //데이터 반환하는 메서드
+      return model.getMedicineList();
+    
+	}
 
 	public static void main(String[] args) throws Exception{
+		SearchMedicine search=new SearchMedicine();			
 		Scanner scan = new Scanner(System.in);
+		String name="";
 		
 		while(true) {
 			StartView.startView();
@@ -18,14 +29,14 @@ public class Controller {
 			case 1: return;
 			case 2: return;
 			case 3: return;
-			case 4: return;
-			case 5: 
-				String name = InputView.inputMedicineName();
-				int quantity = InputView.inputMedicineAmount();
-				UpdateMedicineStock.updateMedicine(name, quantity);
-				break;
+			case 4: name = InputView.inputMedicineName();
+				    int quantity = InputView.inputMedicineAmount();
+				    UpdateMedicineStock.updateMedicine(name, quantity);
+				    break;
+			case 5: search.searchMedicine();   //case5는 조회용이라서 기존 line33~36을 case4로 옮겼어요
+		            break;	
 			case 6: System.out.println("프로그램 종료합니다");
-					return;
+					break;
 			default: System.out.println("잘못된 선택입니다. 다시 선택해 주세요.");
 			}
 		}
--- src/controller/SearchMedicine.java
+++ src/controller/SearchMedicine.java
@@ -1,5 +1,19 @@
 package controller;
+import domain.Medicine;
+import view.InputView;
 
 public class SearchMedicine {
-
+	
+	public void searchMedicine() {
+		Controller control = new Controller();
+		String name=InputView.inputMedicineName();
+		if(control.getMedicineList().containsKey(name)){
+				Medicine m = control.getMedicineList().get(name); 
+					System.out.println("약 이름 : " + m.getName() + "  |  " + "약 가격 : " + m.getPrice() + "  |  "+ "약 재고 수량 : " + m.getAmount());
+			}
+		else {
+			System.out.println("리스트에 없는 약 이름 입니다.");
+		}
+	}
 }
+
