4226e95bbc7958c428d3cba4d431de1387b87964
menzzi <minmin3927@naver.com>
2025-07-04 17:27:44 +0900

    feat: InsertDelete 뷰, 컨트롤러 추가

--- src/controller/InsertDeleteMedicine.java
+++ src/controller/InsertDeleteMedicine.java
@@ -1,5 +1,40 @@
 package controller;
 
+import java.util.HashMap;
+
+import domain.Medicine;
+import model.InsertDeleteModel;
+import model.Model;
+import view.InsertDeleteView;
+
 public class InsertDeleteMedicine {
+	
+	public static void insertMedicine(Model model) {
+		Medicine medicine = InsertDeleteView.inputMedicineInfo();
+		try {
+			if(model.isExist(medicine.getName())) {
+				throw Exception("존재하는 약 입니다.");
+			} 
+			model.insertMedicine(medicine);
+			OutputView.printInsertSuccess(medicine);
+			
+		}catch(Exception e) {
+			OutputView.errorPrint(e.getMessage());
+		}
+		
+	}
+	
+	public static void deleteMedicine(Model model) {
+		String name = InsertDeleteView.inputDeleteMedicineName();
+		try {
+			if(!model.isExist(medicine.getName())) {
+				throw Exception("존재하지 않는 약 입니다.");
+			} 
+			model.deleteMedicine(name);
+			OutputView.printDeleteSuccess(name);
+		}catch (Exception e) {
+			OutputView.errorPrint(e.getMessage());
+		}
+	}
 
 }
--- src/view/InsertDeleteView.java
+++ src/view/InsertDeleteView.java
@@ -1,5 +1,25 @@
 package view;
 
+import domain.Medicine;
+import model.InsertDeleteModel;
+import model.Model;
+
 public class InsertDeleteView {
+	
+	public static Medicine inputInsertMedicineInfo() {
+		System.out.println("삽입할 약품 정보를 입력하세요");
+		String name = InputView.inputMedicineName();
+		int price = InputView.inputMedicinePrice();
+		int amount = InputView.inputMedicineAmount();
+		
+		return new Medicine(name, price, amount);
+	}
+	
+	public static String inputDeleteMedicineName() {
+		System.out.println("삭제할 약품 정보를 입력하세요");
+		String name = InputView.inputMedicineName();
+		
+		return name;
+	}
 
 }
