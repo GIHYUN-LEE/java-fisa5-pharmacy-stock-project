78e1f5c46ebc5c94410e62257d5dacf31bd35b29
shinkibeom <shinkibeoms@naver.com>
2025-07-15 15:18:02 +0900

    feat: Model 클래스  updateMedicine 메소드 구현

--- src/model/Model.java
+++ src/model/Model.java
@@ -41,7 +41,7 @@ public class Model {
         HashMap<String, Medicine> medicineList = model.getMedicineList();
         medicineList.remove(name);
         
-      
+ 
     }
 
     // 약 정보 수정
@@ -62,4 +62,30 @@ public class Model {
         medicineList.remove(oldName);
         medicineList.put(newMed.getName(), newMed);
     }
+    
+    public static void updateMedicine(String name, int newAmount) throws Exception {
+    	  // 약 객체 가져오기
+        Medicine medicine = getMedicineList().get(name);
+
+    	// 약 존재 여부 확인
+        if (!isExist(name)) {
+            throw new Exception("해당 약이 존재하지 않습니다.");
+        }
+
+        // 수량 유효성 검사 및 수량 업데이트
+        if ( medicine.getAmount() + newAmount < 0) {
+            throw new Exception("수량은 음수가 될 수 없습니다.");
+        } else medicine.setAmount(medicine.getAmount()+newAmount);
+
+        
+        // DB 반영 (선택적으로 DAO 호출 가능)
+        boolean success = MedicineDAO.updateAmountMedicine(name, newAmount);
+        if (!success) {
+            throw new Exception("DB 수량 수정 실패");
+        }
+    	
+    }
+    
+    
+    
 }
