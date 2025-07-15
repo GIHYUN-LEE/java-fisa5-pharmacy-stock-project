f905fd4672f631440f4b226c242631b92ef0831b
shinjunsuuu <sjs438@gmail.com>
2025-07-15 15:25:15 +0900

    refactor: StartView Adminview,UserView 세분화

--- src/view/StartView.java
+++ src/view/StartView.java
@@ -5,47 +5,43 @@ import java.util.Scanner;
 import controller.Controller;
 
 public class StartView {
-	
+
 	private static void view() {
-	    System.out.println("╭ ◜◝ ͡ ◜◝ ͡  ◜◝ ͡ ◜◝ ͡  ◜◝ ╮");
-	    System.out.println("        💊  M Y          ");
-	    System.out.println("    P H A R M A C Y  🏥   ");
-	    System.out.println("╰ ◟◞ ͜  ◟ ͜   ◟◞ ͜  ◟ ͜ ◟◞ ╯");
-	    System.out.println("                      O");
-	    System.out.println("                    °");
-		
+		System.out.println("╭ ◜◝ ͡ ◜◝ ͡  ◜◝ ͡ ◜◝ ͡  ◜◝ ╮");
+		System.out.println("        💊  M Y          ");
+		System.out.println("    P H A R M A C Y  🏥   ");
+		System.out.println("╰ ◟◞ ͜  ◟ ͜   ◟◞ ͜  ◟ ͜ ◟◞ ╯");
+		System.out.println("                      O");
+		System.out.println("                    °");
 	}
-	
 
 	public static void main(String[] args) {
-
 		Scanner scan = new Scanner(System.in);
 		Controller controller = new Controller();
 		view();
-		
+
 		while (true) {
 
-			
-		        System.out.println("╔════════════════════════════════════════════════════╗");
-		        System.out.println("║              💊 창고형 약국 시스템 메인 메뉴               ║");
-		        System.out.println("╠════════════════════════════════════════════════════╣");
-		        System.out.println("║  1. 약 삽입       →   새로운 약을 창고에 등록합니다          ║");
-		        System.out.println("║  2. 약 삭제       →   약을 창고에서 제거합니다              ║");
-		        System.out.println("║  3. 약 수량 변경  →   약의 재고 수량을 수정합니다             ║");
-		        System.out.println("║  4. 약 정보 변경  →   약의 이름 및 정보를 수정합니다           ║");
-		        System.out.println("║  5. 약 조회       →   현재 보관 중인 약 목록을 확인합니다      ║");
-		        System.out.println("║  0. 프로그램 종료 →   시스템을 종료합니다                    ║");
-		        System.out.println("╚════════════════════════════════════════════════════╝");
-		        System.out.print("👉 메뉴 번호를 선택하세요: ");
-			
-			
+			System.out.println("===== 창고형 약국 시스템 =====");
+			System.out.print("👉 사용자 이름을 입력해주세요: ");
 
-			int option = scan.nextInt();
+			String name = scan.nextLine();
 			scan.nextLine(); // 버퍼 클리어
-
-			boolean continueProgram = controller.process(option);
-			if (!continueProgram) break;
-
+			
+			String role = Controller.getUserRole(name);
+			
+			if (role == null) {
+                System.out.println("🚫 로그인 실패: 존재하지 않는 사용자입니다.");
+                continue;
+            }
+			if("admin".equals(role)) {
+				AdminView.startAdminView(controller);
+			} else if("user".equals(role)) {
+				UserView.startUserView(controller);
+			} else {
+			    System.out.println("🚫 알 수 없는 권한입니다: ");
+			}
+			
 		}
 	}
 
